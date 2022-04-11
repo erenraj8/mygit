d924a8f9c61c286a9fc07356ee05f48c8faa4053
erenraj8 <raj81194@gmail.com>
2022-04-11 23:52:55 +0530

    my git

---
+++ src/org/git/GitClass.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class GitClass {
+
+	
+	public GitClass() {
+		System.out.println("hi");
+	}
+	
+	public static void main(String[] args) {
+		
+		GitClass g=new GitClass();
+	}
+}
